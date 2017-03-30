137ac067d072382ef95483c6df1e893a2780771b
szhan8 <szhan8@illinois.edu>
2017-03-30 10:43:55 +0800

    add

--- lab9/aes.c
+++ lab9/aes.c
@@ -1,30 +1,40 @@
-/*---------------------------------------------------------------------------
-  --      hello_world.c                                                    --
-  --      Christine Chen                                                   --
-  --      Fall 2013														   --
-  --																	   --
-  --      Updated Spring 2015 											   --
-  --	  Yi Liang														   --
-  --																	   --
-  --      For use with ECE 385 Experiment 9                                --
-  --      UIUC ECE Department                                              --
-  ---------------------------------------------------------------------------*/
-
+/******************************************
+** Advanced Encryption Standard
+** Author: B-Con (b-con@b-con.us)
+** Copyright/Restrictions: GNU GPL
+** Disclaimer: This code is presented "as is" without any garuentees; said author holds
+               liability for no problems rendered by the use of this code.
+** Details: This code is the implementation of the AES algorithm, as specified by the
+            NIST in in publication FIPS PUB 197, availible on the NIST website at
+            http://csrc.nist.gov/publications/fips/fips197/fips-197.pdf .
+******************************************/
 
 #include <stdio.h>
-#include <stdlib.h>
-
-#define to_hw_port (volatile char*) 0x00000000 // actual address here
-#define to_hw_sig (volatile char*) 0x00000000 // actual address here
-#define to_sw_port (char*) 0x00000000 // actual address here
-#define to_sw_sig (char*) 0x00000000 // actual address here
 
 #define uchar unsigned char // 8-bit byte
 #define uint unsigned long // 32-bit word
 
-
 // This is the specified AES SBox. To look up a substitution value, put the first
 // nibble in the first index (row) and the second nibble in the second index (column).
+const uchar aes_sbox[16][16] = {
+   0x63,0x7C,0x77,0x7B,0xF2,0x6B,0x6F,0xC5,0x30,0x01,0x67,0x2B,0xFE,0xD7,0xAB,0x76,
+   0xCA,0x82,0xC9,0x7D,0xFA,0x59,0x47,0xF0,0xAD,0xD4,0xA2,0xAF,0x9C,0xA4,0x72,0xC0,
+   0xB7,0xFD,0x93,0x26,0x36,0x3F,0xF7,0xCC,0x34,0xA5,0xE5,0xF1,0x71,0xD8,0x31,0x15,
+   0x04,0xC7,0x23,0xC3,0x18,0x96,0x05,0x9A,0x07,0x12,0x80,0xE2,0xEB,0x27,0xB2,0x75,
+   0x09,0x83,0x2C,0x1A,0x1B,0x6E,0x5A,0xA0,0x52,0x3B,0xD6,0xB3,0x29,0xE3,0x2F,0x84,
+   0x53,0xD1,0x00,0xED,0x20,0xFC,0xB1,0x5B,0x6A,0xCB,0xBE,0x39,0x4A,0x4C,0x58,0xCF,
+   0xD0,0xEF,0xAA,0xFB,0x43,0x4D,0x33,0x85,0x45,0xF9,0x02,0x7F,0x50,0x3C,0x9F,0xA8,
+   0x51,0xA3,0x40,0x8F,0x92,0x9D,0x38,0xF5,0xBC,0xB6,0xDA,0x21,0x10,0xFF,0xF3,0xD2,
+   0xCD,0x0C,0x13,0xEC,0x5F,0x97,0x44,0x17,0xC4,0xA7,0x7E,0x3D,0x64,0x5D,0x19,0x73,
+   0x60,0x81,0x4F,0xDC,0x22,0x2A,0x90,0x88,0x46,0xEE,0xB8,0x14,0xDE,0x5E,0x0B,0xDB,
+   0xE0,0x32,0x3A,0x0A,0x49,0x06,0x24,0x5C,0xC2,0xD3,0xAC,0x62,0x91,0x95,0xE4,0x79,
+   0xE7,0xC8,0x37,0x6D,0x8D,0xD5,0x4E,0xA9,0x6C,0x56,0xF4,0xEA,0x65,0x7A,0xAE,0x08,
+   0xBA,0x78,0x25,0x2E,0x1C,0xA6,0xB4,0xC6,0xE8,0xDD,0x74,0x1F,0x4B,0xBD,0x8B,0x8A,
+   0x70,0x3E,0xB5,0x66,0x48,0x03,0xF6,0x0E,0x61,0x35,0x57,0xB9,0x86,0xC1,0x1D,0x9E,
+   0xE1,0xF8,0x98,0x11,0x69,0xD9,0x8E,0x94,0x9B,0x1E,0x87,0xE9,0xCE,0x55,0x28,0xDF,
+   0x8C,0xA1,0x89,0x0D,0xBF,0xE6,0x42,0x68,0x41,0x99,0x2D,0x0F,0xB0,0x54,0xBB,0x16
+};
+
 const uchar aes_invsbox[16][16] = {
    0x52,0x09,0x6A,0xD5,0x30,0x36,0xA5,0x38,0xBF,0x40,0xA3,0x9E,0x81,0xF3,0xD7,0xFB,
    0x7C,0xE3,0x39,0x82,0x9B,0x2F,0xFF,0x87,0x34,0x8E,0x43,0x44,0xC4,0xDE,0xE9,0xCB,
@@ -185,191 +195,4 @@ uchar gf_mul[256][6] = {
 uint Rcon[]={0x01000000,0x02000000,0x04000000,0x08000000,0x10000000,0x20000000,
              0x40000000,0x80000000,0x1b000000,0x36000000,0x6c000000,0xd8000000,
              0xab000000,0x4d000000,0x9a000000};
-
-char charToHex(char c)
-{
-	char hex = c;
-
-	if (hex >= '0' && hex <= '9')
-		hex -= '0';
-	else if (hex >= 'A' && hex <='F')
-	{
-		hex -= 'A';
-		hex += 10;
-	}
-	else if (hex >= 'a' && hex <='f')
-	{
-		hex -= 'a';
-		hex += 10;
-	}
-	return hex;
-}
-
-char charsToHex(char c1, char c2)
-{
-	char hex1 = charToHex(c1);
-	char hex2 = charToHex(c2);
-	return (hex1 << 4) + hex2;
-}
-
-// TODO: AES Encryption related function calls
-
-uint RotWord(uint word)
-{
-	uint word1;
-	uint temp;
-	temp = word & 0xFF000000;
-	temp = temp >> 24;
-	word = word << 8;
-	word1 = word1 + temp;
-	return word1;
-}
-
-uint SubWord(uint word)
-{
-	uint word1;
-	uchar i, j, first, second, third, forth;
-	uint temp, temp1, temp2, temp3, temp4;
-	temp = word >> 24;
-	temp1 = word >> 16;
-	temp2 = word >> 8;
-	temp3 = word;
-
-	temp 4 = temp & 0xF0;
-	i = temp4 >> 4;
-	j = temp >> 4;
-	first = aes_invsbox[i][j];
-	
-	temp 4 = temp1 & 0xF0;
-	i = temp4 >> 4;
-	j = temp1 >> 4;
-	second = aes_invsbox[i][j];
-	
-	temp 4 = temp2 & 0xF0;
-	i = temp4 >> 4;
-	j = temp2 >> 4;
-	third = aes_invsbox[i][j];
-	
-	temp 4 = temp3 & 0xF0;
-	i = temp4 >> 4;
-	j = temp3 >> 4;
-	forth = aes_invsbox[i][j];
-
-	word1 = first<<24 + second<<16 + third<<8 + forth;
-	return word1;
-}
-
-void KeyExpansion(uchar* key, uint *word, int Nk)
-{
-	uint temp;
-	int i = 0;
-	while (i < Nk){
-		word[i] = key[4*i]<<24 + key[4*i+1]<<16 + key[4*i+2]<<8 + key[4*i+3]; 
-		i++;
-	}
-	i = Nk;
-	while(i < 10*(4+1)){
-		temp = word[i-1];
-		if(i % Nk == 0)
-			temp = SubWord(Rotword(temp)) ^ Rcon[i/Nk];
-		word[i] = word[i-Nk] ^ temp;
-		i++;
-	}
-}
-
-void AddRoundKey(uchar* state, uchar* rkey)
-{
-	int i;
-	for(i = 0; i < 16; i++){
-		state[i] = state[i] ^ rkey[i];
-	}
-}
-
-void SubBytes(uchar* key)
-{
-	uchar temp, temp1, x, y;
-	int i;
-	for(i = 0; i < 16; i++){
-		temp = key[i];
-		temp1 = temp & 0xF0;
-		x = temp1 >> 4;
-		y = temp >> 4;
-	}
-}
-
-void ShiftRows(uchar* key)
-{
-
-}
-
-int main()
-{
-	int i;
-	unsigned char plaintext[33]; //should be 1 more character to account for string terminator
-	unsigned char key[33];
-
-	// Start with pressing reset
-	*to_hw_sig = 0;
-	*to_hw_port = 0;
-	printf("Press reset!\n");
-	while (*to_sw_sig != 3);
-
-	while (1)
-	{
-		*to_hw_sig = 0;
-		printf("\n");
-
-		printf("\nEnter plain text:\n");
-		scanf ("%s", plaintext);
-		printf ("\n");
-		printf("\nEnter key:\n");
-		scanf ("%s", key);
-		printf ("\n");
-
-		// TODO: Key Expansion and AES encryption using week 1's AES algorithm.
-
-		// TODO: display the encrypted message.
-		printf("\nEncrypted message is\n");
-
-		// Transmit encrypted message to hardware side for decryption. 
-		printf("\nTransmitting message...\n");
-
-		for (i = 0; i < 16; i++)
-		{
-			*to_hw_sig = 1;
-			*to_hw_port = encryptedMsg[i]; // encryptedMsg is your encrypted message
-			// Consider to use charToHex() if your encrypted message is a string. 
-			while (*to_sw_sig != 1);
-			*to_hw_sig = 2;
-			while (*to_sw_sig != 0);
-		}
-		*to_hw_sig = 0;
-		
-		// Transmit encrypted message to hardware side for decryption. 
-		printf("\nTransmitting key...\n");
-
-		//TODO: Transmit key
-
-		printf("\n\n");
-
-		while (*to_sw_sig != 2);
-		printf("\nRetrieving message...\n");
-		for (i = 0; i < 16; ++i)
-		{
-			*to_hw_sig = 1;
-			while (*to_sw_sig != 1);
-			str[i] = *to_sw_port;
-			*to_hw_sig = 2;
-			while (*to_sw_sig != 0);
-		}
-
-		printf("\n\n");
-
-		printf("Decoded message:\n");
-
-		// TODO: print decoded message
-	}
-
-	return 0;
-}
 
