a35c92a12f55061ddce5d95cfc3ac2eb6f874980
annefxh <annefang1@gmail.com>
2017-04-21 11:47:06 -0500

    Update generate.sv

--- final project/generate.sv
+++ final project/generate.sv
@@ -6,7 +6,7 @@ module generate ( input logic game_start,
 always_comb
 begin
   if(game_start)
-    data_out = 3'b111;
+    data_out = 3'b110;
   else
   begin
     data_out[0]=data_in[1] ^ data_in[2];
