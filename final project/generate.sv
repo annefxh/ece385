ff2ff692696d474824acce0206e7e4594e9d5149
annefxh <annefang1@gmail.com>
2017-04-20 22:20:17 -0500

    Update generate.sv

--- final project/generate.sv
+++ final project/generate.sv
@@ -1,14 +1,19 @@
 module generate (input  clk,
                         reset,
-                 input[2:0]  data_in,
-                 output[2:0] data_out
+                 input logic[2:0]  data_in,
+                 output logic[2:0] data_out
 );
 
 always_comb
 begin
-data_out[0]=data_in[1] ^ data_in[2];
-data_out[1]=data_in[0];
-data_out[2]=data_in[1];
+  if(reset)
+    data_out = 3'b000;
+  else
+  begin
+    data_out[0]=data_in[1] ^ data_in[2];
+    data_out[1]=data_in[0];
+    data_out[2]=data_in[1];
+  end
 end
 
 endmodule: generate
