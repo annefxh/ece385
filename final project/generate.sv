083101326487d134e879078c80625a1b959b35ec
annefxh <annefang1@gmail.com>
2017-05-04 08:07:22 -0500

    Update generate.sv

--- final project/generate.sv
+++ final project/generate.sv
@@ -6,12 +6,17 @@ module generation ( input logic reset,
 always_comb
 begin
   if(reset)
-    data_out = 3'b111;
+    data_out = 3'b101;
   else
   begin
+    if(data_in == 3'd3) //special case where output will end up as 3'd7 (background)
+       data_out = 3'b101;
+    else
+      begin
     data_out[0]=data_in[1] ^ data_in[2];
     data_out[1]=data_in[0];
     data_out[2]=data_in[1];
+      end
   end
 end
 
